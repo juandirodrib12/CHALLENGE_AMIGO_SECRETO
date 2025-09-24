cf5c17e670ff61f9d1373bcf6593299cac9a7152
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-09-23 19:38:01 -0500

    Crear un array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Lista para almacenar los nombres
+let amigos = [];
