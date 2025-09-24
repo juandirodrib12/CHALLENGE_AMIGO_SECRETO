d02be04d00ffbffdb403167be4a3e041513e8a4e
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-09-23 19:39:54 -0500

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // Lista para almacenar los nombres
 let amigos = [];
+
+// Función para agregar un nombre a la lista
+function agregarAmigo() {
+    const input = document.getElementById('nombreAmigo');
+    const nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = '';
+}
